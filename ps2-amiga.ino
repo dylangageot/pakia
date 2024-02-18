40d38a60d3a85e959b42f10c43ba3bb5d720f55c
Dylan Gageot <gageot.dylan@gmail.com>
2024-02-18 16:21:40 +0100

    Set up interrupt on falling edge to detect acknowledgement

--- ps2-amiga.ino
+++ ps2-amiga.ino
@@ -3,12 +3,69 @@
 
 #include "ps2.hh"
 
-const size_t AMIGA_CLK_PIN = 0; //8
-const size_t AMIGA_DAT_PIN = 1; //9
+const size_t AMIGA_CLK_PIN = 2;
+const size_t AMIGA_DAT_PIN = 5;
 
 volatile uint8_t amiga_data;
 volatile uint8_t amiga_bit_counter;
 void (*volatile amiga_state)();
+
+inline void amiga_setup_timer()
+{
+    // TCCR1A = (1 << COM1A0);
+    TCCR1B = (1 << WGM12) | (1 << CS10); // | (1 << CS10);
+    OCR1AH = 0x01;
+    OCR1AL = 0x80;
+    TCNT1H = 0;
+    TCNT1L = 0;
+}
+
+inline void amiga_set_pins_as_pull_up()
+{
+    DDRD &= ~((1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN)); // pin D4 as output, toggle by ISR
+    PORTD |= (1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN);   // pull up!
+}
+
+inline bool amiga_frame_is_not_ack()
+{
+    return PORTD & (1 << AMIGA_DAT_PIN);
+}
+
+inline void amiga_set_pins_as_output()
+{
+    DDRD |= (1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN);
+}
+
+inline void amiga_set_bit(uint8_t bit)
+{
+    PORTD = PORTD & ~(1 << AMIGA_DAT_PIN) | (1 << AMIGA_CLK_PIN) | ((bit) ? (1 << AMIGA_DAT_PIN) : 0);
+}
+
+inline void amiga_set_clk_high()
+{
+    PORTD &= ~(1 << AMIGA_CLK_PIN);
+}
+
+inline void amiga_set_clk_low()
+{
+    PORTD |= (1 << AMIGA_CLK_PIN);
+}
+
+inline void amiga_enable_ack_detection_int()
+{
+    EIMSK |= (1 << INT0);
+}
+
+inline void amiga_disable_ack_detection_int()
+{
+    EIMSK ^= (1 << INT0);
+}
+
+inline void amiga_setup_ack_detection()
+{
+    EICRA = (EICRA & ~((1 << ISC00) | (1 << ISC01))) | (FALLING << ISC00);
+    amiga_disable_ack_detection_int();
+}
 
 void setup()
 {
@@ -16,51 +73,48 @@ void setup()
     ps2_fsm.begin();
 
     amiga_state = amiga_idle;
-
-    // timer did it!
-    //TCCR1A = (1 << COM1A0);
-    TCCR1B = (1 << WGM12) | (1 << CS10); // | (1 << CS10);
-    OCR1AH = 0x01;
-    OCR1AL = 0x80;
-    TCNT1H = 0;
-    TCNT1L = 0;
-
-    DDRB &= ~((1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN)); // pin D4 as output, toggle by ISR
-    PORTB |= (1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN);   // pull up!
+    amiga_setup_timer();
+    amiga_setup_ack_detection();
+    amiga_set_pins_as_pull_up();
 }
 
 void amiga_idle() {}
 
-void amiga_transfer_end()
+void amiga_wait_for_ack() {
+};
+
+void amiga_end_transfer()
 {
-    DDRB &= ~((1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN)); // pin D4 as output, toggle by ISR
-    PORTB |= (1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN);   // pull up!
+    amiga_set_pins_as_pull_up();
     TIMSK1 &= ~(1 << OCIE1A);
+
+    //
     amiga_state = amiga_idle;
 }
 
-void amiga_transfer_begin() {
+void amiga_begin_transfer()
+{
     amiga_bit_counter = 0;
-    amiga_state = amiga_position_bit;
+    amiga_set_pins_as_output();
+    amiga_state = amiga_frame_set_dat_bit;
 }
 
-void amiga_position_bit()
+void amiga_frame_set_dat_bit()
 {
-    PORTB = PORTB & ~(1 << AMIGA_DAT_PIN) | (1 << AMIGA_CLK_PIN) | ((amiga_data & (128 >> amiga_bit_counter)) ? (1 << AMIGA_DAT_PIN) : 0);
-    DDRB |= (1 << AMIGA_CLK_PIN) | (1 << AMIGA_DAT_PIN);
-    amiga_state = amiga_set_clk_low;
+    amiga_set_bit(amiga_data & (128 >> amiga_bit_counter));
+    amiga_state = amiga_frame_set_clk_low;
 }
 
-void amiga_set_clk_low()
+void amiga_frame_set_clk_low()
 {
-    PORTB &= ~(1 << AMIGA_CLK_PIN);
-    amiga_state = amiga_set_clk_high;
+    amiga_set_clk_low();
+    amiga_state = amiga_frame_set_clk_high;
 }
 
-void amiga_set_clk_high()
+void amiga_frame_set_clk_high()
 {
-    PORTB |= (1 << AMIGA_CLK_PIN);
-    amiga_state = (++amiga_bit_counter < 8) ? amiga_position_bit : amiga_transfer_end;
+    amiga_set_clk_high();
+    amiga_state = (++amiga_bit_counter < 8) ? amiga_frame_set_dat_bit : amiga_end_transfer;
 }
 
 void send_data_to_amiga(uint8_t data)
@@ -70,7 +124,7 @@ void send_data_to_amiga(uint8_t data)
     {
         // roll out data and reverse bit
         amiga_data = ~((data << 1) | (data >> 7));
-        amiga_state = amiga_transfer_begin;
+        amiga_state = amiga_begin_transfer;
         TIMSK1 = (1 << OCIE1A);
     }
     else
@@ -82,6 +136,10 @@ void send_data_to_amiga(uint8_t data)
 ISR(TIMER1_COMPA_vect)
 {
     (*amiga_state)();
+}
+
+ISR(INT0_vect)
+{
 }
 
 void loop()
