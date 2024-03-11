4d94a2db34c415531e5bdf86dfe24c6b57b8297f
Dylan Gageot <gageot.dylan@gmail.com>
2024-03-11 12:02:07 +0100

    Move all source to same dir

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,14 @@
 
 #include "pins.hh"
 
-ISR(INT0_vect)
+ISR(TIM1_OVF_vect)
 {
-    PORTB ^= pins::amiga::DAT;
+    //PORTB ^= pins::amiga::DAT;
+}
+
+ISR(PCINT0_vect)
+{
+    
 }
 
 int main()
@@ -27,6 +32,36 @@ int main()
     // amiga::CLK and amiga::DAT as output
     DDRB |= pins::amiga::CLK | pins::amiga::DAT;
     PORTB |= pins::amiga::CLK | pins::amiga::DAT;
+
+    // ps2 inihibt
+    // TCCR0A = (1 << WGM01); // CTC
+    // TCCR0B = (1 << CS02) | (1 << CS00); // f_clk_io, no prescaling
+    // TCNT0 = 0;
+    // OCR0A = 155;
+    // TIFR |= (1 << OCF0A);
+    // TIMSK |= (1 << OCIE0A);
+
+    // // amiga bitbanging timer setup
+    // 20u config
+    // TCCR1 = (1 << PWM1A) | (1 << CS10); // f_clk_io, no prescaling
+    // GTCCR &= (1 << TSM) | (1 << PSR0);
+    // TCNT1 = 0;
+    // OCR1C = 159;
+    // TIFR |= (1 << TOV1);
+    // TIMSK |= (1 << TOIE1);
+
+    // 143ms config
+    TCCR1 = (1 << PWM1A) | (1 << CS13) | (1 << CS12) | (1 << CS11); // f_clk_io, no prescaling
+    GTCCR &= (1 << TSM) | (1 << PSR0);
+    TCNT1 = 0;
+    OCR1C = 138;
+    TIFR |= (1 << TOV1);
+    TIMSK |= (1 << TOIE1);
+
+    // ack interrupt
+    GIFR |= (1 << PCIF);
+    GIMSK |= (1 << PCIE);
+    PCMSK |= (1 << PCINT4);
 
     while (1)
     {
