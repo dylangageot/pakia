dbf633d8dced8256764c0740b463be667cbdce91
Dylan Gageot <gageot.dylan@gmail.com>
2024-05-10 15:57:30 +0200

    Refactor ps2 module

--- src/ps2.cc
+++ src/ps2.cc
@@ -9,49 +9,45 @@ namespace ps2 {
 
     struct circular_buffer<uint8_t, 8> scancodes;
 
-    struct fsm {
-        void (*state)();
-        uint8_t buffer;
-        uint8_t counter;
+    void (*state)();
+    uint8_t data;
+    uint8_t counter;
 
-        void begin();
-    } fsm;
-
-    inline static void set_pins_pull_up() {
+    static inline void set_pins_pull_up() {
         DDRB &= ~(pins::ps2::CLK | pins::ps2::DAT);
         PORTB |= pins::ps2::CLK | pins::ps2::DAT;
     }
 
-    inline static void set_clk_low() {
+    static inline void set_clk_low() {
         PORTB &= ~pins::ps2::CLK;
         DDRB |= pins::ps2::CLK;
     }
 
-    inline static void setup_clk_detection_int() {
+    static inline void setup_clk_detection_int() {
         MCUCR = (MCUCR & ~((1 << ISC01) | (1 << ISC00))) | (1 << ISC01);
     }
 
-    inline static void enable_clk_detection_int() {
+    static inline void enable_clk_detection_int() {
         GIFR |= (1 << INTF0);
         GIMSK |= (1 << INT0);
     }
 
-    inline void disable_clk_detection_int() { GIMSK &= ~(1 << INT0); }
+    static inline void disable_clk_detection_int() { GIMSK &= ~(1 << INT0); }
 
-    inline void setup_timer() {
+    static inline void setup_timer() {
         TCCR0A = (1 << WGM01);
         TCCR0B = (1 << CS02) | (1 << CS00);
         TCNT0 = 155;
     }
 
-    inline void enable_timer_int() {
+    static inline void enable_timer_int() {
         TIFR |= (1 << OCF0A);
         TIMSK |= (1 << OCIE0A);
     }
 
-    inline void disable_timer_int() { TIMSK &= ~(1 << OCIE0A); }
+    static inline void disable_timer_int() { TIMSK &= ~(1 << OCIE0A); }
 
-    inline void inhibit() {
+    static inline void inhibit() {
         // lets restore the communications in 20ms
         disable_clk_detection_int();
         set_clk_low();
@@ -65,17 +61,17 @@ namespace ps2 {
             inhibit();
             return;
         }
-        if (scancodes.write(&fsm.buffer)) {
-            fsm.state = process_start_bit;
+        if (scancodes.write(data)) {
+            state = process_start_bit;
         } else {
-            // if circular buffer is full, inhibit keyboard scancode reception
+            // if circular data is full, inhibit keyboard scancode reception
             inhibit();
         }
     }
 
     static void process_parity_bit() {
         uint8_t read_parity = (PINB & pins::ps2::DAT) >> pins::ps2::DAT_PIN;
-        uint8_t computed_parity = fsm.buffer;
+        uint8_t computed_parity = data;
         computed_parity ^= computed_parity >> 4;
         computed_parity ^= computed_parity >> 2;
         computed_parity ^= computed_parity >> 1;
@@ -84,14 +80,13 @@ namespace ps2 {
             inhibit();
             return;
         }
-        fsm.counter++;
-        fsm.state = process_stop_bit;
+        counter++;
+        state = process_stop_bit;
     }
 
     static void process_data_bits() {
-        fsm.buffer |= ((PINB & pins::ps2::DAT) >> pins::ps2::DAT_PIN)
-                      << fsm.counter++;
-        fsm.state = (fsm.counter < 8) ? process_data_bits : process_parity_bit;
+        data |= ((PINB & pins::ps2::DAT) >> pins::ps2::DAT_PIN) << counter++;
+        state = (counter < 8) ? process_data_bits : process_parity_bit;
     }
 
     static void process_start_bit() {
@@ -99,25 +94,24 @@ namespace ps2 {
             inhibit();
             return;
         }
-        fsm.buffer = 0;
-        fsm.counter = 0;
-        fsm.state = process_data_bits;
+        data = 0;
+        counter = 0;
+        state = process_data_bits;
     }
 
-    void begin() { fsm.begin(); }
 
-    void fsm::begin() {
+    void begin() {
         state = process_start_bit;
         set_pins_pull_up();
         setup_clk_detection_int();
         enable_clk_detection_int();
     }
 
-    ISR(INT0_vect) { (*fsm.state)(); }
+    ISR(INT0_vect) { (*state)(); }
 
     ISR(TIM0_COMPA_vect) {
         disable_timer_int();
-        fsm.state = process_start_bit;
+        state = process_start_bit;
         set_pins_pull_up();
         enable_clk_detection_int();
     }
@@ -131,7 +125,7 @@ namespace ps2 {
     }
 
     bool receiver::consume(event &event) {
-        if (scancodes.read(&_last_scancode_fed)) {
+        if (scancodes.read(_last_scancode_fed)) {
             if (_last_scancode_fed == 0xF0) {
                 _is_released = true;
             } else if (_last_scancode_fed == 0xE0) {
