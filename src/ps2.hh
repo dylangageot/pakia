6df53028507b53a24b19361a17fb9bbf3f588f49
Dylan Gageot <gageot.dylan@gmail.com>
2024-05-12 17:02:04 +0200

    Add prototype function for ps2::send command

--- src/ps2.hh
+++ src/ps2.hh
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdint.h>
 
 namespace ps2 {
 
@@ -7,7 +8,7 @@ namespace ps2 {
     enum event_kind { PRESSED = 0, RELEASED = 1 };
 
     struct event {
-        enum event_kind event_kind;
+        event_kind event_kind;
         uint8_t scancode;
     };
 
@@ -21,5 +22,18 @@ namespace ps2 {
         bool _is_e0_prefixed;
         uint8_t _last_scancode_fed;
     };
+
+    enum command {
+        RESET = 0xFF,
+        RESEND = 0xFE,
+        SET_DEFAULT = 0xF6,
+        DISABLE = 0xF5,
+        ENABLE = 0xF4,
+        SET_TYPEMATIC_RATE_DELAY = 0xF3,
+        ECHO = 0xEE,
+        SET_RESET_LEDS = 0xED
+    };
+
+    void send(command command, uint8_t args = 0);
 
 } // namespace ps2
